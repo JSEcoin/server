17960b65f3525d15fac05dc717328a0d71c12f83
jamesmc <james@mediacambridge.com>
2018-08-11 15:35:30 +0200

    uniqueConfirmationCode on email welcome link check

--- routes/confirm.js
+++ routes/confirm.js
@@ -17,32 +17,36 @@ router.get('/:uid/:confirmcode', function(req, res) {
 		}
 		if (credentials.confirmCode === req.params.confirmcode) {
 			res.send('<html><script>alert("Thanks for confirming your account"); window.location = "https://platform.jsecoin.com/";</script></html>');
-			JSE.jseDataIO.getVariable('account/'+credentials.uid,function(account) {
-				if (account.confirmed !== true && account.source === 'referral') {
-					const tier1 = 'US,DE,NL,SG,HK,CH,SE,IE,NO,';
-					const tier2 = 'FR,CA,GB,JP,KR,AU,CZ,IT,ES,LT,FI,AT,BE,NZ,IL,DK,SK,SI,TW,PT,';
-					const tier3 = 'CN,RU,UA,PL,BR,BG,RO,TH,MY,HU,ZA,TR,GR,AR,LV,IN,VN,MX,';
-					let referralPayout = 0;
-					if (tier1.indexOf(account.geo) > -1) referralPayout = 500;
-					if (tier2.indexOf(account.geo) > -1) referralPayout = 200;
-					if (tier3.indexOf(account.geo) > -1) referralPayout = 100;
-					if (referralPayout > 0) {
-					//if (account.geo === 'US' || account.geo === 'CA' || account.geo === 'GB' || account.geo === 'IE' || account.geo === 'AU' || account.geo === 'NZ' || account.geo === 'ZA' || account.geo === 'DE' || account.geo === 'FR' || account.geo === 'CH' || account.geo === 'SE' || account.geo === 'NO' || account.geo === 'FI' || account.geo === 'BE' || account.geo === 'NL' || account.geo === 'LU' || account.geo === 'DK' || account.geo === 'AT') {
-						if (account.duplicate === null || typeof account.duplicate === 'undefined') {
-							JSE.jseFunctions.referral(account.campaign,account.content,referralPayout);
+			const uniqueConfirmationCode = JSE.jseFunctions.randString(12);
+			JSE.jseDataIO.setVariable('account/'+credentials.uid+'/confirmed', uniqueConfirmationCode);
+			setTimeout(function(checkUID,checkConfirmationCode) {
+				JSE.jseDataIO.getVariable('account/'+checkUID,function(account) {
+					if (account.confirmed === checkConfirmationCode && account.source === 'referral') {
+						const tier1 = 'US,DE,NL,SG,HK,CH,SE,IE,NO,';
+						const tier2 = 'FR,CA,GB,JP,KR,AU,CZ,IT,ES,LT,FI,AT,BE,NZ,IL,DK,SK,SI,TW,PT,';
+						const tier3 = 'CN,RU,UA,PL,BR,BG,RO,TH,MY,HU,ZA,TR,GR,AR,LV,IN,VN,MX,';
+						let referralPayout = 0;
+						if (tier1.indexOf(account.geo) > -1) referralPayout = 500;
+						if (tier2.indexOf(account.geo) > -1) referralPayout = 200;
+						if (tier3.indexOf(account.geo) > -1) referralPayout = 100;
+						if (referralPayout > 0) {
+						//if (account.geo === 'US' || account.geo === 'CA' || account.geo === 'GB' || account.geo === 'IE' || account.geo === 'AU' || account.geo === 'NZ' || account.geo === 'ZA' || account.geo === 'DE' || account.geo === 'FR' || account.geo === 'CH' || account.geo === 'SE' || account.geo === 'NO' || account.geo === 'FI' || account.geo === 'BE' || account.geo === 'NL' || account.geo === 'LU' || account.geo === 'DK' || account.geo === 'AT') {
+							if (account.duplicate === null || typeof account.duplicate === 'undefined') {
+								JSE.jseFunctions.referral(account.campaign,account.content,referralPayout);
+							} else {
+								console.log('Declined Referral Duplicate Account: '+account.confirmed);
+								JSE.jseFunctions.referral(account.campaign,'Declined Duplicate Account Details',0.01); // Anything with Declined in will get value set to 0.01
+							}
 						} else {
-							console.log('Declined Referral Duplicate Account: '+account.confirmed);
-							JSE.jseFunctions.referral(account.campaign,'Declined Duplicate Account Details',0.01); // Anything with Declined in will get value set to 0.01
+							JSE.jseFunctions.referral(account.campaign,'Declined Region '+account.geo,1); // declined regions go through at 1 JSE
+							console.log('Declined Referral GEO: '+account.geo);
 						}
 					} else {
-						JSE.jseFunctions.referral(account.campaign,'Declined Region '+account.geo,1); // declined regions go through at 1 JSE
-						console.log('Declined Referral GEO: '+account.geo);
+						console.log('Declined Referral Source: '+account.source+' Confirmed: '+account.confirmed);
 					}
-				} else {
-					console.log('Declined Referral Source: '+account.source+' Confirmed: '+account.confirmed);
-				}
-				setTimeout(function() { JSE.jseDataIO.setVariable('account/'+credentials.uid+'/confirmed', true); }, 1000); // set timeout, shouldn't be needed but trying to fix referrals bug where a lot are getting declined because they are already confirmed
-			});
+					setTimeout(function() { JSE.jseDataIO.setVariable('account/'+checkUID+'/confirmed', true); }, 1000); // set timeout, shouldn't be needed but trying to fix referrals bug where a lot are getting declined because they are already confirmed
+				});
+			}, Math.floor(Math.random() * 60000), credentials.uid, uniqueConfirmationCode); // do this after a random interval up to 60 seconds
 		} else {
 			res.send('<html>Error: Confirmation code not recognised</html>');
 		}
