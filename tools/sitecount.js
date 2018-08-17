6b325df85052646f2d9239fc4128720f0b3af2d3
jamesmc <james@mediacambridge.com>
2018-08-17 20:21:04 +0200

    Publisher advertising site count script

---
+++ tools/sitecount.js
@@ -0,0 +1,16 @@
+/* Quick script to see how many sites we have opted in to display advertising */
+
+const siteIDs = require('./../data/siteids.json');
+
+let marketing = 0;
+Object.keys(siteIDs).forEach(function(i) {
+  if (siteIDs[i]) {
+    let maxSiteCount = 0;
+    Object.keys(siteIDs[i]).forEach(function(i2) {
+      maxSiteCount +=1;
+      if (siteIDs[i][i2] && siteIDs[i][i2].m === true && maxSiteCount < 100 && siteIDs[i][i2].s !== 'Platform Mining' && siteIDs[i][i2].s !== 'undefined') marketing += 1;
+    });
+  }
+});
+
+console.log('Sites opted in for advertising: '+marketing);
