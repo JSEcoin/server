dbf0b2e1bb7d8ca114f0cf74c806be2e3ca45ef9
jamesmc <james@mediacambridge.com>
2018-11-19 17:25:54 +0000

    Update to affiliate rates and tiers

--- routes/confirm.js
+++ routes/confirm.js
@@ -54,13 +54,12 @@ router.get('/:uid/:confirmcode', function(req, res) {
 			setTimeout(function(checkUID,checkConfirmationCode,checkIP) {
 				JSE.jseDataIO.getVariable('account/'+checkUID,function(account) {
 					if (account.uniqueConfirmationCode === checkConfirmationCode && account.source === 'referral' && account.confirmed === false) {
-						const tier1 = 'US,DE,NL,SG,HK,CH,SE,IE,NO,';
-						const tier2 = 'FR,CA,GB,JP,KR,AU,CZ,IT,ES,LT,FI,AT,BE,NZ,IL,DK,SK,SI,TW,PT,';
-						const tier3 = 'CN,RU,UA,PL,BR,BG,RO,TH,MY,HU,ZA,TR,GR,AR,LV,IN,VN,MX,';
+						const tier1 = 'US,DE,SG,HK,CH,SE,IE,NO,FR,CA,GB,JP,KR,AU,CZ,IT,ES,LT,FI,AT,BE,NZ,IL,DK,SK,SI,PT,';
+						//const tier2 = 'FR,CA,GB,JP,KR,AU,CZ,IT,ES,LT,FI,AT,BE,NZ,IL,DK,SK,SI,TW,PT,';
 						let referralPayout = 0;
-						if (tier1.indexOf(account.geo) > -1) referralPayout = 500;
-						if (tier2.indexOf(account.geo) > -1) referralPayout = 200;
-						if (tier3.indexOf(account.geo) > -1) referralPayout = 100;
+						if (tier1.indexOf(account.geo) > -1) referralPayout = 300;
+						//if (tier2.indexOf(account.geo) > -1) referralPayout = 200;
+						//if (tier3.indexOf(account.geo) > -1) referralPayout = 100;
 						if (referralPayout > 0) {
 							if (account.duplicate === null || typeof account.duplicate === 'undefined') {
 								JSE.jseFunctions.realityCheck(checkIP, function(goodIPTrue) {
