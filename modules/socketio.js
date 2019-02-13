789be7a8ba3e44f52bfafc84b90972e9ee104e48
jamesmc <james@mediacambridge.com>
2019-02-13 20:49:25 +0000

    bug fix on selectedAds validation data undefined

--- modules/socketio.js
+++ modules/socketio.js
@@ -245,9 +245,9 @@ const jseSocketIO = {
 							// double check currentRating (last var in visitorTensorArray) > 50 server-side once enough volume
 							jseMachineLearning.recordPublisherMLData(pubID,visitorTensor);
 							jseLottery.credit(pubID,siteID,subID,'validate');
-							if (ipCount === 0) {
+							if (ipCount === 0 && selectedAds) {
 								for (let i = 0; i < selectedAds.length; i+=1) {
-									console.log('### LogAdStat V ### '+selectedAds[i].size);
+									//console.log('### LogAdStat V ### '+selectedAds[i].size);
 									jseAds.logAdStat(selectedAds[i],'v');
 								}
 							}
