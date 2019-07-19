ad791e18fe9e775a72b055744dea8806a1e55af0
James Bachini <james@jsecoin.com>
2019-07-19 23:24:33 +0200

    check more uniques than coins on pub minining

--- modules/socketio.js
+++ modules/socketio.js
@@ -252,7 +252,12 @@ const jseSocketIO = {
 							const visitorTensor = jseMachineLearning.calculateInitialRating(jseTrack);
 							// double check currentRating (last var in visitorTensorArray) > 50 server-side once enough volume
 							jseMachineLearning.recordPublisherMLData(pubID,visitorTensor);
-							jseLottery.credit(pubID,siteID,subID,'validate');
+							const safeKey = JSE.jseDataIO.genSafeKey(siteID);
+							JSE.getVariable('siteIDs/'+pubID+'/'+safeKey,function(siteData) {
+								if (siteData && siteData.u > siteData.c / 100) { // reduce this variable over time
+									jseLottery.credit(pubID,siteID,subID,'validate');
+								}
+							});
 							if (selectedAds && !JSE.publisherIPsValidated[socket.realIP]) {
 								for (let i = 0; i < selectedAds.length; i+=1) {
 									//console.log('### LogAdStat V ### '+selectedAds[i].size);
